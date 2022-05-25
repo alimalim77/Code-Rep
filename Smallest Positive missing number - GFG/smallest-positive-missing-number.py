9bab7a7a2d9801aa28bd105511e1d6be00406b93
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2022-05-25 16:11:15 +0530

    Added solution - LeetHub

---
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find the smallest positive number missing from the array.
+    def missingNumber(self,arr,n):
+        #Your code here
+        li = [-1] * 1000000
+        for i in arr:
+            li[i] = 0
+        for i in range(1,1000001):
+            if li[i] < 0:
+                return i
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            
+            ob=Solution()
+            print(ob.missingNumber(arr,n))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
