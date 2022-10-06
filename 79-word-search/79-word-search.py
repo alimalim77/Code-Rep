5a5bcf00ea2d86d0c6c216b555cf9cff49116b17
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2022-10-06 23:00:35 +0530

    Time: 4738 ms (82.92%), Space: 13.9 MB (92.98%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -7,17 +7,12 @@ class Solution:
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            if  min(r, c) < 0 or r >= row or c >= col or word[i] != board[r][c] or (r, c) in path :
+            if  r < 0 or c < 0 or r >= row or c >= col or word[i] != board[r][c] or (r, c) in path :
                 return False
             
             temp = board[r][c]
             board[r][c] = '.'
-            res = (
-                dfs(r + 1, c, i + 1)
-                or dfs(r - 1, c, i + 1)
-                or dfs(r, c + 1, i + 1)
-                or dfs(r, c - 1, i + 1)
-            )
+            res = dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1)
 
             
             board[r][c] = temp
