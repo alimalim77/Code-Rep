3dff2141f6ac9e0b2d221c3e581d7517845b8dd1
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2023-06-17 00:44:43 +0530

    Time: 98 ms (52.75%), Space: 54.8 MB (60.78%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,21 @@
+function findMaxAverage(nums, k) {
+  let win_start = 0;
+  let win_end = 0;
+  let total = 0;
+  
+  let res = Number.NEGATIVE_INFINITY;
+  while (win_end < nums.length) {
+    if (win_end - win_start + 1 > k) {
+      total -= nums[win_start];
+      win_start += 1;
+    }
+    total += nums[win_end];
+    
+    if (win_end - win_start + 1 === k) {
+      let avg = total / k;
+      res = Math.max(res, avg);
+    }
+    win_end += 1;
+  }
+  return res;
+}
