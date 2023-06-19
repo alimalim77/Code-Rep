ac8a63ffa581b3311331c03149dc116758ac206e
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2023-06-20 00:27:11 +0530

    Time: 56 ms (28.30%), Space: 16.4 MB (43.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,11 @@
-# t o d a y - S
-# d a s t o y - T
-from collections import Counter
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        set1 = Counter(s)
-        set2 = Counter(t)
+        # Two For Loop - We will iterate over one then another
+        result = 0 
+        for i in s: # CORING the first string
+            result = result ^ ord(i)
         
-        for i in set2:
-            if set1[i] != set2[i] or i not in set1:
-                return i
-        
-        
-        
+        for i in t: # XORING for cancellation of first string
+            result = result ^ ord(i)
+            
+        return chr(result)
