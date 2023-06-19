ee37bae81061bb2593e6b8b5559a90d42f1e4852
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2023-06-20 00:27:22 +0530

    Time: 47 ms (71.10%), Space: 16.3 MB (43.47%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,7 +2,7 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         # Two For Loop - We will iterate over one then another
         result = 0 
-        for i in s: # CORING the first string
+        for i in s: # XORING the first string
             result = result ^ ord(i)
         
         for i in t: # XORING for cancellation of first string
