b100f9095c8ac92354585059545ce5d6bb47070f
Mohammad Alim <52186295+alimalim77@users.noreply.github.com>
2023-06-20 00:29:46 +0530

    Time: 51 ms (53.15%), Space: 16.4 MB (11.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         # Two For Loop - We will iterate over one then another
-        result = 0 
-        for i in s: # XORING the first string
+        result = 0 # O(1)
+        for i in s: # XORING the first string #O(n)
             result = result ^ ord(i)
         
-        for i in t: # XORING for cancellation of first string
+        for i in t: # XORING for cancellation of first string #O(n)
             result = result ^ ord(i)
             
         return chr(result)
